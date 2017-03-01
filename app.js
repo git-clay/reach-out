791936d8669471b907635f6a16aee229afe6a696
git-clay <ce.smith5525@gmail.com>
2017-03-01 13:29:07 -0700

    basic server

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-var results = document.getElementById('results');
+var express 	= require('express'),
+	app			= express(),
+	bodyParser	= require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var	fName='',
+	lName='',
+	company='',
+	phone='',
+	email='',
+	linkedin='',
+	facebook='',
+	twitter='',
+	github='';
+	
+
 
 console.log()
 function submitForm(){
-	var form = document.getElementById('form');
-	var text = [];
-	for(var i=0;i<form.length;i++){
-		text.push({
-			name:form.elements[i].name,
-			value: form.elements[i].value
-		})
-	}
-	console.log(text)
+	var form = document.getElementById('form').elements;
+	fName=form.fName.value,
+	lName=form.lName.value,
+	company=form.company.value,
+	phone=form.phone.value,
+	email=form.email.value,
+	linkedin=form.linkedin.value,
+	facebook=form.facebook.value,
+	twitter=form.twitter.value,
+	github=form.github.value;
+
+
 }
