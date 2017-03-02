c3647d43d9b103f7e95ee4c5834842a627e2cd97
git-clay <ce.smith5525@gmail.com>
2017-03-01 18:42:28 -0700

    emails are being checked and verified to be real

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express 	= require('express'),
 	app			= express(),
 	bodyParser	= require('body-parser'),
 	request		= require('request'),
-	co 			= require('co'),
-	mx			= require('validate-email-dns');
+	verifier = require('email-verify');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static('./'));
+require('dotenv').load(); // require and load dotenv
 
 var	fName='',
 	lName='',
@@ -22,12 +22,11 @@ var	fName='',
 app.post('/api',function(req,res){
 	var ob = req.body;
 	email= ob.email;
-	co.wrap(mx)(email)
-		.then(function(correct){
-			res.send(correct)
-		})
-	})
-
+	console.log(email)
+	verifier.verify( email, function( err, info ){
+  console.log(info)
+});
+})
 
 
 app.listen(process.env.PORT || 3000, function(){
